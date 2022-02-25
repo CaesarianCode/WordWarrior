1a02cf1a86d209f4dbea98cabf6a56dc867e5713
Ravi Laakso <>
2022-02-25 16:44:52 +0000

    Loading Screen Created

--- A__title_screen.py
+++ A__title_screen.py
@@ -1,7 +1,31 @@
 import random
-print("WORD WARRIOR")
-for i in range(24):
+print(" |  /|  /  __   __  __|   |  /|  /  __     __  __ .   __   __")
+print("  |/  |/  |__| |   |__|    |/  |/  |__|_  |   |   |  |__|  |")
+for space in range(2):
   print()
-continue_function = input("press c to continue    ")
-if continue_function == "c" or "C":
-  import B__intro
+print("            (    (    )")
+print("       (                      )")
+print("   (           (       )")
+print("(   (                                 )")
+print("        (     (              )")
+print("      ((                    )")
+print("  (       )(     (      )")
+print("      \  \  |      |          .")
+print("       \  \|      |            |")
+print("        \  |      |             |")
+print("          |      |              ~")
+print("          |      |     #   (  ) /")
+print("         | @    |      | /     /")
+print("         |      |      |/|    |")
+print("         |      |        \    /")
+print("         |      |         |  |")
+for space in range(6):
+  print()
+for contunue in range(10000000):
+  continue_function = input("press c to continue    ")
+  if continue_function == "c":
+    import loading_screen
+    break
+  if continue_function == "C":
+    import loading_screen
+    break
---
+++ loading_screen.py
@@ -0,0 +1,45 @@
+import time
+import random
+for loading_screen in range(random.randint(1,5)):
+  for space in range(39):
+    print()
+  print("                          L")
+  time.sleep(0.25)
+  for space in range(39):
+    print()
+  print("                          Lo")
+  time.sleep(0.25)
+  for space in range(39):
+    print()
+  print("                          Loa")
+  time.sleep(0.25)
+  for space in range(39):
+    print()
+  print("                          Load")
+  time.sleep(0.25)
+  for space in range(39):
+    print()
+  print("                          Loadi")
+  time.sleep(0.25)
+  for space in range(39):
+    print()
+  print("                          Loadin")
+  time.sleep(0.25)
+  for space in range(39):
+    print()
+  print("                          Loading")
+  time.sleep(0.25)
+  for space in range(39):
+    print()
+  print("                          Loading.")
+  time.sleep(0.75)
+  for space in range(39):
+      print()
+  print("                          Loading..")
+  time.sleep(0.75)
+  for space in range(39):
+      print()
+  print("                          Loading...")
+  time.sleep(0.75)
+  for space in range(39):
+      print()
